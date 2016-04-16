1c8f2bd90c4a9e41c80c9afb97c415fc29b5b07d
wtlwd <wt232625@gmail.com>
2016-04-15 21:18:19 -0400

    Initial commit

---
+++ app/src/main/java/com/course/example/justamap/MapsActivity.java
@@ -0,0 +1,108 @@
+package com.course.example.justamap;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap myMap;
+    private static final LatLng BENTLEY = new LatLng(42.3889167, -71.2208033);
+    private static final float zoom = 14.0f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+
+        myMap.setMyLocationEnabled(true);
+
+        //myMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        myMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        //myMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        //myMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        
+        myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(BENTLEY, zoom));
+
+        myMap.setOnMarkerClickListener(
+                new GoogleMap.OnMarkerClickListener() {
+
+                    public boolean onMarkerClick(Marker m) {
+                        String title = m.getTitle();
+                        String snip = m.getSnippet();
+                        Toast.makeText(getApplicationContext(), title + "\n" + snip, Toast.LENGTH_LONG).show();
+                        return true;
+                    }
+                }
+        );
+
+        myMap.setOnMapLongClickListener(
+                new GoogleMap.OnMapLongClickListener() {
+                    public void onMapLongClick(LatLng point) {
+                        Toast.makeText(getApplicationContext(), "Long Tap", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #myMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (myMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            myMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (myMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #myMap} is not null.
+     */
+    private void setUpMap() {
+        myMap.addMarker(new MarkerOptions()
+                .position(BENTLEY)
+                .title("Bentley University")
+                .snippet("Population: 5,000")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
+    }
+
+}
